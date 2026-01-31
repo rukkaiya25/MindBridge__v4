17326265c31947fa6faa749e7b6ad00fd79734f2
rukkaiya25 <rukkaiya.mca25@bmsce.ac.in>
2026-02-01 01:01:43 +0530

    putting ca certificate

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,18 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
+const sslConfig = process.env.DB_CA_CERT
+  ? { ca: process.env.DB_CA_CERT, rejectUnauthorized: true }
+  : undefined;
+
 const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 
-  ssl: {
-    rejectUnauthorized: true
-  },
+  ssl: sslConfig,
 
   waitForConnections: true,
   connectionLimit: 10,
